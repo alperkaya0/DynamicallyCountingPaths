8b9a191cb1d2cf2558893df33124b97b3462650f
Alper Kaya <73350743+alperkaya0@users.noreply.github.com>
2021-07-24 19:33:19 +0300

    Update Count Path Non-Dynamic.py

--- Count Path Non-Dynamic.py
+++ Count Path Non-Dynamic.py
@@ -1,16 +1,11 @@
 
-solvedOnes = {}
 def countPaths(rows,columns,start):
     if start[0]>rows-1 or start[1]>columns-1:
         path = 0
     elif start[0] == rows-1 and start[1] == columns-1:
         path = 1
     else:
-        if (tuple([start[0]+1,start[1]]) in solvedOnes) and (tuple([start[0]+1,start[1]+1]) in solvedOnes) and (tuple([start[0],start[1]+1]) in solvedOnes):
-            path = solvedOnes[tuple([start[0]+1,start[1]])] + solvedOnes[tuple([start[0],start[1]+1])] + solvedOnes[tuple([start[0]+1,start[1]+1])]
-        else:
-            path = countPaths(rows,columns,[start[0]+1,start[1]]) + countPaths(rows,columns,[start[0],start[1]+1]) + countPaths(rows,columns,[start[0]+1,start[1]+1])
-    solvedOnes[tuple(start)] = path
+        path = countPaths(rows,columns,[start[0]+1,start[1]]) + countPaths(rows,columns,[start[0],start[1]+1]) + countPaths(rows,columns,[start[0]+1,start[1]+1])
     return path
 
 
